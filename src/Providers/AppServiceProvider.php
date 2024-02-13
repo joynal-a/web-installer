0b6c4eba70a612197e55544054d05901c24ad192
Joynal Abedin <joynal.a@razinsoft.com>
2024-02-13 11:55:20 +0600

    init commit

---
+++ src/Providers/AppServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Abedin\Boiler\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        // $this->registerNewModelCommand();
+    }
+
+    // protected function registerNewModelCommand(): void
+    // {
+    //     $this->app->bind('command.make:model', MakeModel::class);
+    //     $this->commands(['command.make:model']);
+    // }
+
+}
