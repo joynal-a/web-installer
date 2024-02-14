0e511fedf2c5946da3bc000c8116f5ca5646e4e3
Joynal Abedin <joynal.a@razinsoft.com>
2024-02-14 15:52:22 +0600

    test run

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Abedin\WebInstaller\Controllers\WelcomeController;
 use Illuminate\Routing\Route;
 
 Route::group(['prefix' => 'install', 'as' => 'installer.', 'middleware' => ['web']], function () {
-    Route::get('/', [WelcomeController::class, 'welcome'])->name('welcome');
+    Route::get('/', [WelcomeController::class, 'welcome'])->name('well');
 });
--- src/Providers/AppServiceProvider.php
+++ src/Providers/AppServiceProvider.php
@@ -6,19 +6,40 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+
     /**
      * Register any application services.
      */
     public function register(): void
     {
-        // $this->registerNewModelCommand();
+        $this->loadRoutesFrom(__DIR__.'/../../routes/web.php');
     }
 
-    // protected function registerNewModelCommand(): void
-    // {
-    //     $this->app->bind('command.make:model', MakeModel::class);
-    //     $this->commands(['command.make:model']);
-    // }
+    public function boot()
+    {
+        //
+    }
+
+    protected function publishFiles()
+    {
+        $this->publishes([
+            __DIR__.'/../../config/installer.php' => base_path('config/installer.php'),
+        ], 'web-installer');
+
+        // $this->publishes([
+        //     __DIR__.'/../assets' => public_path('installer'),
+        // ], 'web-installer');
+
+        $this->publishes([
+            __DIR__.'/../../resources/views' => base_path('resources/views/vendor/installer'),
+        ], 'web-installer');
+    }
 
 }
