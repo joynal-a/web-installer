22232b198a509be111fd48e4ef6982644ca9789e
Joynal Abedin <joynal.a@razinsoft.com>
2024-02-14 15:54:43 +0600

    test run

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Abedin\WebInstaller\Controllers\WelcomeController;
-use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'install', 'as' => 'installer.', 'middleware' => ['web']], function () {
     Route::get('/', [WelcomeController::class, 'welcome'])->name('well');
