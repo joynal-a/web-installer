5fa2918ab2744f9c029a2c20e6acc9b5f4c1fc1f
Joynal Abedin <joynal.a@razinsoft.com>
2024-02-19 23:54:09 +0600

    connect requirment page

--- resources/views/requirment.blade.php
+++ resources/views/requirment.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="col-6 col-md-4 ms-auto d-flex align-items-center me-5" style="height: 100vh;">
     <div class="card w-100 pt-3" style="overflow: hidden;border-bottom: none;">
-        <h2 class="text-center mt-3">Check Permissions</h2>
+        <h2 class="text-center mt-3">Check Server Requerments</h2>
 
         <p class="fs-7 text-center">
             <strong class="text-danger">You will need to know the following items before proceeding.</strong>
--- src/Controllers/RequirementController.php
+++ src/Controllers/RequirementController.php
@@ -5,10 +5,13 @@ namespace Abedin\WebInstaller\Controllers;
 class RequirementController extends Controller
 {
      /**
-     * Display the installer welcome page.
+     * Display the installer requirement page.
      */
     public function index()
     {
-
+        return match($this->isPublish){
+            true => view('vendor.web-installer.requirement'),
+            default => view('joynala.web-installer::requirement')
+        };
     }
 }
