c867fd1b33c53b3704d1903d969a48180f385f27
Joynal Abedin <joynal.a@razinsoft.com>
2024-02-20 13:36:33 +0600

    use lib file

--- src/Controllers/RequirementController.php
+++ src/Controllers/RequirementController.php
@@ -2,87 +2,24 @@
 
 namespace Abedin\WebInstaller\Controllers;
 
+use Abedin\WebInstaller\Lib\Managers\RequirementManager;
+
 class RequirementController extends Controller
 {
-    private $minPhpVersion = '8.0.0';
-
+    public function __construct(
+        private $manager = RequirementManager::class
+    ){}
      /**
      * Display the installer requirement page.
      */
     public function index()
     {
-        $phpSupportInfo = $this->checkPHPversion(config('installer.minPhpVersion'));
-        $extensions = $this->check(config('installer.php_extensions'));
+        $phpSupportInfo = $this->manager->checkPHPversion(config('installer.minPhpVersion'));
+        $extensions = $this->manager->check(config('installer.php_extensions'));
 
         return match($this->isPublish){
             true => view('vendor.web-installer.requirement', compact('phpSupportInfo', 'extensions')),
             default => view('joynala.web-installer::requirement', compact('phpSupportInfo', 'extensions'))
         };
-    }
-
-    /**
-     * Check server requirements.
-     *
-     * @param array $requirements
-     * @return array
-     */
-    public function check(array $requirements): array
-    {
-        $results = [
-            'requirements' => [],
-            'next' => true
-        ];
-
-        foreach ($requirements as $requirement) {
-            $results['requirements'][$requirement] = true;
-
-            if (! extension_loaded($requirement)) {
-                $results['requirements'][$requirement] = false;
-                $results['next'] = false;
-            }
-        }
-
-        return $results;
-    }
-
-    /**
-     * Check PHP version requirement.
-     * @return array
-     */
-    public function checkPHPversion(string $minPhpVersion = null): array
-    {
-        $minPhpVersion = $minPhpVersion ?? $this->minPhpVersion;
-        $currentPhpVersion = $this->getPhpVersionInfo();
-        $status = false;
-
-        if (version_compare($currentPhpVersion['current'], $minPhpVersion) >= 0) {
-            $status = true;
-        }
-
-        $phpStatus = [
-            'last' => $currentPhpVersion['last'],
-            'current' => $currentPhpVersion['current'],
-            'minimum' => $minPhpVersion,
-            'status' => $status,
-        ];
-
-        return $phpStatus;
-    }
-
-    /**
-     * Get current Php version information.
-     *
-     * @return array
-     */
-    private static function getPhpVersionInfo(): array
-    {
-        $currentVersionFull = PHP_VERSION;
-        preg_match("#^\d+(\.\d+)*#", $currentVersionFull, $filtered);
-        $currentVersion = $filtered[0];
-
-        return [
-            'last' => $currentVersionFull,
-            'current' => $currentVersion,
-        ];
     }
 }
---
+++ src/Lib/Managers/RequirementManager.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Abedin\WebInstaller\Lib\Managers;
+
+class RequirementManager 
+{
+    private $minPhpVersion = '8.0.0';
+
+    /**
+     * Check server requirements.
+     *
+     * @param array $requirements
+     * @return array
+     */
+    public function check(array $requirements): array
+    {
+        $results = [
+            'requirements' => [],
+            'next' => true
+        ];
+
+        foreach ($requirements as $requirement) {
+            $results['requirements'][$requirement] = true;
+
+            if (! extension_loaded($requirement)) {
+                $results['requirements'][$requirement] = false;
+                $results['next'] = false;
+            }
+        }
+
+        return $results;
+    }
+
+    /**
+     * Check PHP version requirement.
+     * @return array
+     */
+    public function checkPHPversion(string $minPhpVersion = null): array
+    {
+        $minPhpVersion = $minPhpVersion ?? $this->minPhpVersion;
+        $currentPhpVersion = $this->getPhpVersionInfo();
+        $status = false;
+
+        if (version_compare($currentPhpVersion['current'], $minPhpVersion) >= 0) {
+            $status = true;
+        }
+
+        $phpStatus = [
+            'last' => $currentPhpVersion['last'],
+            'current' => $currentPhpVersion['current'],
+            'minimum' => $minPhpVersion,
+            'status' => $status,
+        ];
+
+        return $phpStatus;
+    }
+
+    /**
+     * Get current Php version information.
+     *
+     * @return array
+     */
+    private static function getPhpVersionInfo(): array
+    {
+        $currentVersionFull = PHP_VERSION;
+        preg_match("#^\d+(\.\d+)*#", $currentVersionFull, $filtered);
+        $currentVersion = $filtered[0];
+
+        return [
+            'last' => $currentVersionFull,
+            'current' => $currentVersion,
+        ];
+    }
+}
+
