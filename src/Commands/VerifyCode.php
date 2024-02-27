d584cf5b61685147d7a3e9e391e2506579ddef26
Joynal Abedin <joynal.a@razinsoft.com>
2024-02-27 19:37:31 +0600

    solve app issue

--- src/Commands/VerifyCode.php
+++ src/Commands/VerifyCode.php
@@ -4,7 +4,7 @@ namespace Abedin\WebInstaller\Commands;
 
 use Illuminate\Console\Command;
 
-class GenerateVerifyCode extends Command
+class VerifyCode extends Command
 {
     /**
      * The name and signature of the console command.
