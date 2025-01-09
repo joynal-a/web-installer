89e2281a6833c1ec372471e4175504f3718a31d4
Joynal Abedin <abedin.dev@gmail.com>
2025-01-09 18:25:49 +0600

    change background image

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,7 +13,7 @@
         }
 
         .main {
-            background: url("https://i.ibb.co/SPrLMYw/background.png");
+            background: url("https://razinsoft.com/image/Installer.jpg");
             width: 100%;
             height: 100vh;
             background-repeat: no-repeat;
