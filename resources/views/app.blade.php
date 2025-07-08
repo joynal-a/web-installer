0cd2ac9a744d551d2dd7b84499ced8962f9fcb35
Joynal Abedin <abedin.dev@gmail.com>
2025-07-08 17:49:58 +0600

    update loader and BG images

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,7 +13,7 @@
         }
 
         .main {
-            background: url("https://razinsoft.com/image/Installer.jpg");
+            background: url("https://i.ibb.co/2YjmfvQY/Installer.jpg");
             width: 100%;
             height: 100vh;
             background-repeat: no-repeat;
--- resources/views/update/upload.blade.php
+++ resources/views/update/upload.blade.php
@@ -5,7 +5,7 @@
     <div class="col-6 col-md-4 ms-auto d-flex align-items-center me-5" style="height: 100vh;" id="mainCard">
         <div class="card w-100 pt-3" style="overflow: hidden;border-bottom: none;">
             <div class="loader" id="loader" style="display: none">
-                <img src="https://razinsoft.com/image/cog_loader.gif" alt="cog" border="0">
+                <img src="https://i.ibb.co/9mWzCWCt/cog-loader.gif" alt="cog" border="0">
             </div>
 
             <h2 class="text-center mt-3">Ready To Upgrade</h2>
